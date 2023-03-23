86f95c97331dffe6e24441d2d3af310e5e3974fd
Mark Litvinenko <mark.litvinenko@heathmont.net>
2023-03-23 17:10:55 +0200

    text update

--- src/MessageService.ts
+++ src/MessageService.ts
@@ -19,42 +19,37 @@ export class MessageService {
         this.client = client;
     }
 
+    getEmoji(emojiName) {
+        return this.client?.emojis?.cache?.find(emoji => emoji.name === emojiName) || null
+    }
+
     public aggressiveReplies(message) {
+        let finalString = "";
         if (message.content.toLowerCase().includes("кролик")) {
-            const rabbitDance = this.client?.emojis?.cache?.find(emoji => emoji.name === "rabbitdance") || null;
-            if (rabbitDance) {
-                message.channel.send(rabbitDance.toString());
-            }
-            else {
-                console.log("emoji not found")
+            const emoji = this.getEmoji("rabbitdance");
+            if (emoji) {
+                finalString = finalString + emoji.toString() + "\n";
             }
         }
         if (message.content.toLowerCase().includes("по жопе")) {
-            const rabbitSlap = this.client?.emojis?.cache?.find(emoji => emoji.name === "rabbitbuttslap") || null;
-            if (rabbitSlap) {
-                message.channel.send(rabbitSlap.toString());
-            }
-            else {
-                console.log("emoji not found")
+            const emoji = this.getEmoji("rabbitbuttslap");
+            if (emoji) {
+                finalString = finalString + emoji.toString() + "\n";
             }
         }
         if (message.content.toLowerCase().includes("накажи")) {
-            const rabbitSlapRed = this.client?.emojis?.cache?.find(emoji => emoji.name === "rabbitredslap") || null;
-            if (rabbitSlapRed) {
-                message.channel.send(rabbitSlapRed.toString());
-            }
-            else {
-                console.log("emoji not found")
+            const emoji = this.getEmoji("rabbitredslap");
+            if (emoji) {
+                finalString = finalString + emoji.toString() + "\n";
             }
         }
-
         if (message.content.toLowerCase().includes("сиськи?")) {
-            message.reply('Сиськи Лир, но лучше проверить карту осады.');
+            finalString = finalString + "Сиськи Лир, но лучше проверить карту осады.\n"
         }
-        else if (message.content.toLowerCase().includes("кто вор")) {
-            message.reply('Древняя китайская мудрость: \n1) Ба обвиняет Бу: Бу\n2) Бу обвиняет Бу: Ба\n3) Бо обвиняет Бу: Бо');
+        if (message.content.toLowerCase().includes("кто вор")) {
+            finalString = finalString + 'Древняя китайская мудрость: \n1) Ба обвиняет Бу: Бу\n2) Бу обвиняет Бу: Ба\n3) Бо обвиняет Бу: Бо\n';
         }
-        else if (message.content.includes("<@1088130726597234791>")) {//Bot highlighted
+        if (message.content.includes("<@1088130726597234791>")) {//Bot highlighted
             let firstMessage = "";
             if(message.author.id === "266214524225323008") { //Killer
                 firstMessage = "Держи морковку.\n\n";
@@ -69,10 +64,9 @@ export class MessageService {
                 firstMessage = "АААААААааааааааааааАААААААААаааААА!\n\n";
             }
             else if(message.author.id === "205731886184660992") {//Owner
-                message.reply('Я не смею перечить своему господину.');
-                return;
+                firstMessage = 'Я устал, босс.\n';
             }
-            message.reply(firstMessage + selfEvent[Math.floor(Math.random() * selfEvent.length)])
+            message.reply(finalString + firstMessage + selfEvent[Math.floor(Math.random() * selfEvent.length)])
         }
     }
 }
