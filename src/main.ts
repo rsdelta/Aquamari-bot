c0c1103f4be9e3ad08b6cdf5e8c812d82e6c56bb
Mark Litvinenko <mark.litvinenko@heathmont.net>
2023-03-26 13:01:56 +0200

    audioplayer draft

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { config } from 'dotenv';
 import { jokes } from './strings/jokes';
 import {guild_events, guild_events_mobile, GuildEvent} from './strings/schedule';
 import { scheduleJob } from 'node-schedule';
-import { Client, GatewayIntentBits, Collection, Routes, REST, SlashCommandBuilder, ChannelType } from 'discord.js';
+import { Client, GatewayIntentBits, Collection, Routes, REST, SlashCommandBuilder, ChannelType, EmbedBuilder } from 'discord.js';
+import {QueryType , Player} from 'discord-player'
 import { MessageService } from "./MessageService";
 
 const TIMEZONE_OFFSET = 2;
@@ -11,7 +12,8 @@ let globalTimeout = null;
 let rest = null;
 let commands = null;
 
-const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages ]});
+const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates ]});
+
 client["commands"] = new Collection();
 
 config();
@@ -37,7 +39,7 @@ function startTimedEvent() {
 	}, 1000);
 }
 
-function initClientEvents() {
+async function initClientEvents() {
 	client.on('messageCreate', (message) => {
 		if (message?.author?.id === "1088130726597234791") {
 			return;
@@ -77,6 +79,9 @@ function initClientEvents() {
 					const randomNumber = Math.floor(Math.random() * 100) + 1;
 					interaction.reply({ content: `${userString} бросает кубик: [${randomNumber}] оч.`});
 					break;
+				case 'play':
+					playAudio(interaction);
+					break;
 			}
 
 		}
@@ -115,6 +120,31 @@ function scheduleMessageCommand() {
 		.toJSON();
 }
 
+function playAudioCommand() {
+	return new SlashCommandBuilder()
+			.setName("play")
+			.setDescription("play a song from YouTube.")
+			.addSubcommand(subcommand =>
+				subcommand
+					.setName("search")
+					.setDescription("Searches for a song and plays it")
+					.addStringOption(option =>
+						option.setName("searchterms").setDescription("search keywords").setRequired(true)
+					)
+			)
+			.addSubcommand(subcommand =>
+				subcommand
+					.setName("playlist")
+					.setDescription("Plays a playlist from YT")
+					.addStringOption(option => option.setName("url").setDescription("the playlist's url").setRequired(true))
+			)
+			.addSubcommand(subcommand =>
+				subcommand
+					.setName("song")
+					.setDescription("Plays a single song from YT")
+					.addStringOption(option => option.setName("url").setDescription("the song's url").setRequired(true))
+			)
+}
 function rollCommand() {
 	return new SlashCommandBuilder().setName('roll').setDescription('Roll dice').toJSON();
 }
@@ -122,7 +152,8 @@ function rollCommand() {
 function initCommands() {
 	commands = [
 		scheduleMessageCommand(),
-		rollCommand()
+		rollCommand(),
+		playAudioCommand()
 	];
 }
 
@@ -139,4 +170,105 @@ async function main() {
 	catch (err) {
 		console.log(err);
 	}
+}
+
+
+async function playAudio(interaction) {
+
+		if (!interaction.member.voice.channel) return interaction.reply("You need to be in a Voice Channel to play a song.");
+		const player = new Player(client, {
+			ytdlOptions: {
+				filter: "audioonly"
+			}});
+		if (!player || !player.createQueue) {
+			return;
+		}
+
+	player.on('error', (queue, error) => {
+		console.log(error);
+		console.log(queue);
+		console.log(`Error emitted from the queue ${error.message}`);
+	});
+
+	player.on('connectionError', (queue, error) => {
+		console.log(error);
+		console.log(`Error emitted from the connection ${error.message}`);
+	});
+
+		// Create a play queue for the server
+	const queue = await player.createQueue(interaction.guild, {autoSelfDeaf: false});
+
+		// Wait until you are connected to the channel
+		if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+
+		let embed = new EmbedBuilder();
+
+		if (interaction.options.getSubcommand() === "song") {
+			let url = interaction.options.getString("url");
+
+			// Search for the song using the discord-player
+			const result = await player.search(url, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.AUTO
+			});
+
+			// finish if no tracks were found
+			if (result.tracks.length === 0)
+				return interaction.reply("No results");
+
+			// Add the track to the queue
+			const song = result.tracks[0];
+			await queue.addTrack(song);
+			embed
+				.setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+				.setThumbnail(song.thumbnail)
+				.setFooter({ text: `Duration: ${song.duration}`})
+
+		}
+		else if (interaction.options.getSubcommand() === "playlist") {
+
+			// Search for the playlist using the discord-player
+			let url = interaction.options.getString("url");
+			const result = await player.search(url, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.YOUTUBE_PLAYLIST
+			});
+
+			if (result.tracks.length === 0)
+				return interaction.reply(`No playlists found with ${url}`);
+
+			const playlist = result.playlist;
+			await queue.addTracks(result.tracks);
+			embed
+				.setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** have been added to the Queue`)
+				.setThumbnail(playlist.thumbnail)
+
+		}
+		else if (interaction.options.getSubcommand() === "search") {
+
+			let url = interaction.options.getString("searchterms")
+			const result = await player.search(url, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.AUTO
+			});
+
+			if (result.tracks.length === 0)
+				return interaction.editReply("No results")
+
+			const song = result.tracks[0];
+			await queue.addTrack(song);
+			embed
+				.setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+				.setThumbnail(song.thumbnail)
+				.setFooter({ text: `Duration: ${song.duration}`})
+		}
+
+		if (!queue.playing) await queue.play().then(event => {
+
+		});
+
+
+		await interaction.reply({
+			embeds: [embed]
+		})
 }
